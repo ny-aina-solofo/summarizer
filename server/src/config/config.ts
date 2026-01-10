1a273ce5ad21aac8ff316974792735833d8e8278
ny-aina-solofo <nyainarandrianarisolo@gmail.com>
2026-01-10 17:24:19 +0300

    refactor(server) : secure configuration by removing hardcoded defaults

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -3,17 +3,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 interface Config {
-  urlAPI: string;
-  baseUrl: string;
+  urlAPI: string | undefined;
+  baseUrl: string | undefined;
   port: number;
-  nodeEnv: string;
+  nodeEnv: string | undefined;
 }
 
 const config: Config = {
-    urlAPI : process.env.URL_API || 'http://localhost:4200',
-    baseUrl : process.env.BASE_URL || '/summarizer-api',
-    port: Number(process.env.PORT) || 3000,
-    nodeEnv: process.env.NODE_ENV || 'dev',
+    urlAPI : process.env.URL_API,
+    baseUrl : process.env.BASE_URL,
+    port: Number(process.env.PORT),
+    nodeEnv: process.env.NODE_ENV,
 };
 
 export default config;
--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -2,19 +2,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 interface DbConfig {
-  host: string;
-  username: string;
-  password: string;
-  database: string;
+  host: string | undefined;
+  username: string | undefined;
+  password: string | undefined;
+  database: string | undefined;
   port: number;
 }
 
 const dbConfig: DbConfig = {
-  host: process.env.HOST_CONNECTION || "localhost",
-  username: process.env.USER_CONNECTION || "postgres",
-  password: process.env.PASSWORD_CONNECTION || "postgres",
-  database: process.env.DATABASE_CONNECTION || "summarizer_db",
-  port: Number(process.env.PORT_CONNECTION) || 5433,
+  host: process.env.HOST_CONNECTION,
+  username: process.env.USER_CONNECTION,
+  password: process.env.PASSWORD_CONNECTION,
+  database: process.env.DATABASE_CONNECTION,
+  port: Number(process.env.PORT_CONNECTION),
 };
 
 export default dbConfig;
